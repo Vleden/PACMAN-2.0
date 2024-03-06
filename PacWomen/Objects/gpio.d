c45289c20ce84a2d6c8f4ed14ed2eb93c6b53cf4
Vleden <117099446+Vleden@users.noreply.github.com>
2024-03-06 17:20:28 +0100

    Add files via upload

---
+++ PacWomen/Objects/gpio.d
@@ -0,0 +1,17 @@
+.\objects\gpio.o: Sources\HARD\GPIO.c
+.\objects\gpio.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.2.0\Device\Include\stm32f10x.h
+.\objects\gpio.o: .\RTE\_SPACE-DEFENDER\RTE_Components.h
+.\objects\gpio.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\core_cm3.h
+.\objects\gpio.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\gpio.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\cmsis_compiler.h
+.\objects\gpio.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\cmsis_armcc.h
+.\objects\gpio.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.2.0\Device\Include\system_stm32f10x.h
+.\objects\gpio.o: .\RTE\Device\STM32F103ZG\stm32f10x_conf.h
+.\objects\gpio.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.2.0\Device\StdPeriph_Driver\inc\stm32f10x_gpio.h
+.\objects\gpio.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.2.0\Device\Include\stm32f10x.h
+.\objects\gpio.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.2.0\Device\StdPeriph_Driver\inc\stm32f10x_rcc.h
+.\objects\gpio.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.2.0\Device\StdPeriph_Driver\inc\misc.h
+.\objects\gpio.o: .\Headers\HARD\Registers.h
+.\objects\gpio.o: .\Headers\HARD\GPIO.h
+.\objects\gpio.o: .\Headers\GAME\game.h
+.\objects\gpio.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
